46a24a3308595f22b88bb6d25c27cc7096a728b5
vedantkherade <152008775+vedantkherade@users.noreply.github.com>
2025-10-06 22:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const openBtn = document.getElementById("openModal"); 
+const modal = document.getElementById("modal"); 
+const closeBtn = document.querySelector(".close-modal");
+
+openBtn.addEventListener("click", () => {
+	modal.style.display = "block";
+})
+
+closeBtn.addEventListener("click", () => {
+	modal.style.display = "none";
+})
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
